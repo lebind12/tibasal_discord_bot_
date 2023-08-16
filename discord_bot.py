1888930ea0fdc6752ba3671fc64baee013e3b1f7
woolee <lebind12@naver.com>
2023-08-16 23:01:19 +0900

    change privilage

--- discord_bot.py
+++ discord_bot.py
@@ -7,19 +7,19 @@ class MyClient(discord.Client):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
-        print(message.channel.name)
         print(f'Message from {message.author}: {message.content}')
         command = message.content
-        if command == "Bot command : 어제 소식 가져오기" and message.channel.name == "ㅎㅎ":
-            await message.channel.send("어제 이후의 멤버들의 카페소식을 가져옵니다.")
-            res = get_data()
-            if len(res) == 0:
-                await message.channel.send("어제자 이후로 생성된 글이 없습니다.")
-            for r in res:
-                name, url = r
-                await message.channel.send(name)
-                await message.channel.send(url)
-            await message.channel.send("끝.")
+        if command == "Bot command : 어제 소식 가져오기":
+            if message.author == "eaglekop" or message.author == "mm9372":
+                await message.channel.send("어제 이후의 멤버들의 카페소식을 가져옵니다.")
+                res = get_data()
+                if len(res) == 0:
+                    await message.channel.send("어제자 이후로 생성된 글이 없습니다.")
+                for r in res:
+                    name, url = r
+                    await message.channel.send(name)
+                    await message.channel.send(url)
+                await message.channel.send("끝.")
         
         
 intents = discord.Intents.default()
